7139fecb2ff646e65d092d39d78324889e880289
EasterGeorge <eastergeorge830@gmail.com>
2020-05-01 23:27:11 +0300

    done

--- day1.py
+++ day1.py
@@ -1,3 +1,10 @@
 
 def twofer(name):
-    # Enter your code here. Read input from STDIN. Print output to STDOUT
+    if (name==""):
+        print("One for "+ name +", one for me.")
+    else:
+        print("One for you, one for me.")
+        
+if __name__ == '__main__':
+    name = input("Input your name")
+    twofer(name)
